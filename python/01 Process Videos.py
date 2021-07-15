3bf37c889597ac443afdb5066498b8ad56c6b60f
andyweaves <andyweaves>
2021-07-15 14:16:53 +0000

    Slight modifications

--- python/01 Process Videos.py
+++ python/01 Process Videos.py
@@ -3,6 +3,14 @@
 
 # COMMAND ----------
 
+#dbutils.fs.put("dbfs:/aweaver/init_ffmpeg.sh", """
+#sudo apt-get update
+#sudo apt install -qq -y ffmpeg > ffmpeg_install.log
+#/databricks/python/bin/pip install opencv-python 
+#""", True)
+
+# COMMAND ----------
+
 import dlt
 
 from pyspark.sql.functions import *
@@ -18,7 +26,36 @@ dbutils.fs.mkdirs(IMG_OUTPUT_DIR)
 
 # COMMAND ----------
 
+@pandas_udf(StringType())
+def extract_images(x: pd.Series) -> pd.Series:
+
+  import cv2
+  from os.path import basename
+
+  def extract_image(y):
+
+    count = 0
+    v = cv2.VideoCapture(y)
+    filename = basename(y).replace(FORMAT, "")
+    success, img = v.read()
+    success = True
+
+    while success:
+      v.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
+      success, img = v.read()
+      if success:
+        output_file = f"/{IMG_OUTPUT_DIR.replace(':', '')}/{filename}_frame_{count}.jpg"
+        cv2.imwrite(output_file, img)
+      count += 1
+
+    return output_file
+
+  return x.apply(extract_image)
+
+# COMMAND ----------
+
 @dlt.table(
+  name="bronze_videos_raw",
   comment="Raw videos to be processed"
 )
 def videos_raw():
@@ -31,41 +68,19 @@ def videos_raw():
 # COMMAND ----------
 
 @dlt.table(
-  comment="Videos split into frames"
+  comment="Videos split into frames",
+  name="silver_videos_split"
 )
 def split_videos():
-  
-  @pandas_udf(StringType())
-  def extract_images(x: pd.Series) -> pd.Series:
 
-    import cv2
-    from os.path import basename
-
-    def extract_image(y):
-
-      count = 0
-      v = cv2.VideoCapture(y)
-      filename = basename(y).replace(FORMAT, "")
-      success, img = v.read()
-      success = True
-
-      while success:
-        v.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
-        success, img = v.read()
-        if success:
-          output_file = f"/{IMG_OUTPUT_DIR.replace(':', '')}/{filename}_frame_{count}.jpg"
-          cv2.imwrite(output_file, img)
-        count += 1
-
-      return output_file
-
-    return x.apply(extract_image)
-
-  return (dlt.read("videos_raw").withColumn("output_path", extract_images(col("path"))))
+  return (dlt.read("bronze_videos_raw").withColumn("output_path", extract_images(col("path"))))
 
 # COMMAND ----------
 
-@dlt.table
+@dlt.table(
+  comment="Images loaded into Delta Lake",
+  name="gold_image_frames"
+)
 def images():
   return (
   spark.readStream.format("cloudFiles").option("cloudFiles.format", "binaryFile").option("recursiveFileLookup", "true") \
