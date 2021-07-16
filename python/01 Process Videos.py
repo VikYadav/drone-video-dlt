518013918f7423613bd439d2a6c5ac9d4b10c729
andyweaves <andyweaves>
2021-07-16 08:30:14 +0000

    Add DQ checks

--- python/01 Process Videos.py
+++ python/01 Process Videos.py
@@ -3,7 +3,7 @@
 
 # COMMAND ----------
 
-#dbutils.fs.put("dbfs:/aweaver/init_ffmpeg.sh", """
+#dbutils.fs.put("dbfs:/aweaver/init_ffmpeg_1.sh", """
 #sudo apt-get update
 #sudo apt install -qq -y ffmpeg > ffmpeg_install.log
 #/databricks/python/bin/pip install opencv-python 
@@ -15,8 +15,6 @@ import dlt
 
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from pyspark.sql.functions import pandas_udf, PandasUDFType
-from pyspark.sql.types import StringType
 import pandas as pd
 
 IMG_OUTPUT_DIR = "dbfs:/stanford_drone/images/"
@@ -58,22 +56,25 @@ def extract_images(x: pd.Series) -> pd.Series:
   name="bronze_videos_raw",
   comment="Raw videos to be processed"
 )
+@dlt.expect_all({"valid_length": "length > 0", "valid_generated_fields": "fuse_path IS NOT NULL"})
 def videos_raw():
   return (
     spark.read.format("binaryFile") \
     .option("pathGlobFilter", "*{}".format(".mov")) \
     .load("dbfs:/databricks-datasets-private/ML/stanford_drone/videos/") \
-    .withColumn("path", regexp_replace("path", "dbfs:/", "/dbfs/")).select("path", "modificationTime", "length")) 
+    .withColumn("fuse_path", regexp_replace("path", "dbfs:/", "/dbfs/")) \
+    .select("path", "modificationTime", "length", "fuse_path")) 
 
 # COMMAND ----------
 
 @dlt.table(
   comment="Videos split into frames",
-  name="silver_videos_split"
+  name="video_frames"
 )
+@dlt.expect_all({"valid_length": "length > 0", "valid_generated_fields": "image_path IS NOT NULL"})
 def split_videos():
 
-  return (dlt.read("bronze_videos_raw").withColumn("output_path", extract_images(col("path"))))
+  return (dlt.read("bronze_videos_raw").withColumn("image_path", extract_images(col("fuse_path"))))
 
 # COMMAND ----------
 
@@ -81,9 +82,10 @@ def split_videos():
   comment="Images loaded into Delta Lake",
   name="gold_image_frames"
 )
+@dlt.expect_all({"valid_length": "length > 0", "has_content": "content IS NOT NULL", "valid_generated_fields": "content_hash IS NOT NULL"})
 def images():
   return (
   spark.readStream.format("cloudFiles").option("cloudFiles.format", "binaryFile").option("recursiveFileLookup", "true") \
   .option("pathGlobFilter", "*.jpg") \
-  .load(IMG_OUTPUT_DIR) 
+  .load(IMG_OUTPUT_DIR).withColumn("content_hash", sha2("content", 512))
   )
